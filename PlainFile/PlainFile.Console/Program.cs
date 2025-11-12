9e92ab54e5c33c54cc5b5ceda203d2fc5a4fc66c
jarlenandres <jaarojas@gmai.com>
2025-11-12 14:03:08 -0500

    Log class creation

--- PlainFile/PlainFile.Console/Program.cs
+++ PlainFile/PlainFile.Console/Program.cs
@@ -4,13 +4,17 @@ var textFile = new SimpleTextFile("data.txt");
 var lines = textFile.ReadAllLines().ToList();
 var opc = string.Empty;
 
+using var logger = new LogWriter("app.log");
+logger.WriteLog("INFO", "Application started.");
+
 do
 {
     opc = Menu();
-    switch(opc)
+    switch (opc)
     {
         case "1":
             Console.WriteLine("Contents:");
+            logger.WriteLog("INFO", "The file was displayed.");
             foreach (var line in lines)
             {
                 Console.WriteLine(line);
@@ -22,21 +26,27 @@ do
             if (!string.IsNullOrEmpty(newLine))
             {
                 lines.Add(newLine);
+                Console.WriteLine("Line added.");
+                logger.WriteLog("INFO", $"A new line was added: {newLine}");
             }
             else
             {
                 Console.WriteLine("No line added.");
+                logger.WriteLog("WARNING", "Attempted to add an empty line.");
             }
             break;
         case "3":
             textFile.WriteAllLines(lines.ToArray());
             Console.WriteLine("File saved.");
+            logger.WriteLog("INFO", "The file was saved.");
             break;
         case "0":
             Console.WriteLine("Exiting...");
+            logger.WriteLog("INFO", "Application exited.");
             break;
         default:
             Console.WriteLine("Invalid option. Please try again.");
+            logger.WriteLog("ERROR", $"Invalid menu option selected: {opc}");
             break;
     }
 } while (opc != "0");
---
+++ PlainFile/PlainFile.Core/LogWriter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlainFile.Core 
+{
+    public class LogWriter : IDisposable
+    {
+        private readonly StreamWriter _writer;
+
+        public LogWriter(string path)
+        {
+            _writer = new StreamWriter(path, append: true)
+            {
+                AutoFlush = true
+            };
+        }
+
+        public void WriteLog(string level, string message)
+        {
+            var timetamp = DateTime.Now.ToString("s");
+            _writer.WriteLine($"[{timetamp}] - [{level}] - {message}");
+        }
+
+        public void Dispose()
+        {
+            _writer.Dispose();
+        }
+    }
+}
